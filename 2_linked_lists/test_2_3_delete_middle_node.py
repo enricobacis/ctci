069c7af8031d3fc88295da46a47967eb310467c6
enricobacis <enrico.bacis@gmail.com>
2017-01-04 13:31:54 +0100

    add 2_linked_lists/test_2_3_delete_middle_node.py

---
+++ 2_linked_lists/test_2_3_delete_middle_node.py
@@ -0,0 +1,20 @@
+"""
+Implement an algorithm to delete a node in the middle of a singly linked
+list, given only access to that node.
+"""
+
+from linkedlist import Node
+
+
+def delete_middle_node(node):
+    """time: O(1) space: O(1)"""
+    # we actually clone node.next into node
+    node.value = node.next.value
+    node.next = node.next.next
+
+
+def test_delete_middle_node():
+    head = Node.from_iterable('abcdef')
+    c = head.next.next
+    delete_middle_node(c)
+    assert head.to_list() == list('abdef')
