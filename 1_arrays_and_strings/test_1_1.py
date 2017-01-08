84d24cf957ecdc2803170a608505ed680b17444d
enricobacis <enrico.bacis@gmail.com>
2017-01-08 19:02:11 +0100

    use fn fixture in test_1_1.py

--- 1_arrays_and_strings/test_1_1.py
+++ 1_arrays_and_strings/test_1_1.py
@@ -19,6 +19,7 @@ def is_unique_2(string):
 
 def is_unique_3(lst):
     """time: O(n*logn) space: O(1). It modifies the list in place."""
+    if not isinstance(lst, list): lst = list(lst)
     lst.sort()
     for i in xrange(len(lst) - 1):
         if lst[i] == lst[i + 1]:
@@ -26,9 +27,9 @@ def is_unique_3(lst):
     return True
 
 
-def test_is_unique():
-    assert is_unique('abcd') == is_unique_2('abcd') == is_unique_3(list('abcd')) == True
-    assert is_unique('aacd') == is_unique_2('aacd') == is_unique_3(list('aacd')) == False
-    assert is_unique('abbd') == is_unique_2('abbd') == is_unique_3(list('abbd')) == False
-    assert is_unique('abcc') == is_unique_2('abcc') == is_unique_3(list('abcc')) == False
-    assert is_unique('abca') == is_unique_2('abca') == is_unique_3(list('abca')) == False
+def test_is_unique(fn):
+    assert fn('abcd') == True
+    assert fn('aacd') == False
+    assert fn('abbd') == False
+    assert fn('abcc') == False
+    assert fn('abca') == False
