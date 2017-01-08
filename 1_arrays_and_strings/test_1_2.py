ee21196fa3d30e442a1991a8ca62ea4432c1122f
enricobacis <enrico.bacis@gmail.com>
2017-01-08 19:04:11 +0100

    use fn fixture in test_1_2.py

--- 1_arrays_and_strings/test_1_2.py
+++ 1_arrays_and_strings/test_1_2.py
@@ -11,13 +11,15 @@ def check_permutation(a, b):
 
 def check_permutation_2(a, b):
     """time: O(n*logn) space: O(1). It modifies the lists in place."""
+    if not isinstance(a, list): a = list(a)
+    if not isinstance(b, list): b = list(b)
     if len(a) != len(b): return False
     a.sort(); b.sort()
     return a == b
 
 
-def test_check_permutation():
-    assert check_permutation('abcd', 'abc') == check_permutation_2(list('abcd'), list('abc')) == False
-    assert check_permutation('abcd', 'abce') == check_permutation_2(list('abcd'), list('abce')) == False
-    assert check_permutation('abcd', 'acbd') == check_permutation_2(list('abcd'), list('acbd')) == True
-    assert check_permutation('abab', 'baba') == check_permutation_2(list('abab'), list('baba')) == True
+def test_check_permutation(fn):
+    assert fn('abcd', 'abc')  == False
+    assert fn('abcd', 'abce') == False
+    assert fn('abcd', 'acbd') == True
+    assert fn('abab', 'baba') == True
